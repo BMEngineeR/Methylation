3024910d256df40402704b5c5fa2d103ddcc2a88
BMEngineeR <cyz931123@gmail.com>
2019-07-22 09:26:52 -0400

    completed version of DMR

--- script/DifferentialMethylationRegion.R
+++ script/DifferentialMethylationRegion.R
@@ -37,17 +37,23 @@ my.sample.condition.2<-my.sample.condition[match(gsub("MAA","",my.filename),my.s
 my.group<-my.sample.condition.2$Group
 my.methylation.cov.list<-list(MAA4130,MAA4308,MAA4382,MAA4414,MAA4494,MAA4617,MAA4660,MAA4788,MAA4807,MAA5190)
 # checkpoint
-save(my.methylation.cov.list,file = "my.methylation.cov.list")
-load("my.methylation.cov.list")
+# save(my.methylation.cov.list,file = "my.methylation.cov.list")
+# load("my.methylation.cov.list")
 my.object<-makeBSseqData(my.methylation.cov.list,my.group)
-save(my.object,file="my.object")
-load("./my.object")
+# save(my.object,file="my.object")
+ load("./my.object")
 # RRBS does not recommend smoothing process
 my.normal<-my.group[grep("^Normal",my.group)]
 my.alzheimer<-my.group[grep("^Al",my.group)]
 DMLTest<- DMLtest(my.object,group1 = my.normal,group2 = my.alzheimer)
+#############################
+#############checkpoint######
+#############################
 dmls <-callDML(DMLTest,p.threshold = 0.05)
+save(dmls,file = 'dmls')
+Sys.time()
 dmrs <- callDMR(DMLTest,p.threshold = 0.05)
+
 ###########################
 ####  check point##########
 ###########################
@@ -59,12 +65,17 @@ head(dmrs)
 gene.obj=readTranscriptFeatures("GENE.location.Info.txt")
 gene.annotate<-annotateWithGeneParts(as(dmrs,"GRanges"),gene.obj)
 head(gene.annotate@dist.to.TSS)
-# add annotated gene to 
-new.DMR.v1<-cbind.data.frame(dmrs,gene.annotate@dist.to.TSS[,c(2,3,4)])
-
-
-
-
+# add annotated gene to new.DMR.v1
+new.DMR.v1<-cbind.data.frame(dmrs,gene.annotate@dist.to.TSS[,c(2,3,4)],gene.annotate@members)
+# extract ref. name from new.DMR.v1
+gene.REFSEQ<-gsub(".[0-9]+$","",new.DMR.v1$feature.name)
+# switch name from refseq to gene symbol
+gene.SYMBOL<-AnnotationDbi::select(org.Hs.eg.db,keys = gene.REFSEQ,
+                                   keytype = "REFSEQ",
+                                   columns = "SYMBOL")
+# insery gene symbol to new DMR
+new.DMR.v2<-cbind.data.frame(new.DMR.v1[,1:11],gene.symbol=gene.SYMBOL$SYMBOL,new.DMR.v1[,12:15])
+write.csv(new.DMR.v2,file="DMR.csv",quote = F,row.names = F)
 
 
 
